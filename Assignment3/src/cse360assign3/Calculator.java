c707a8eac6585a04f052704a2ced4ded59d44d39
katelyna <katelyna@katelyna-HP>
2016-02-23 15:25:09 -0700

    Version 2- updated methods and tests

--- Assignment3/src/cse360assign3/Calculator.java
+++ Assignment3/src/cse360assign3/Calculator.java
@@ -4,32 +4,48 @@ package cse360assign3;
 public class Calculator {
 
 	private int total;
+	String output = "0";
 	
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
 	}
 	
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	public void add (int value) {
-		
+		total = total + value;
+		output = output + " + ";
+		output = output + value;
 	}
 	
 	public void subtract (int value) {
-		
+		total = total - value;
+		output = output + " - ";
+		output = output + value;
 	}
 	
 	public void multiply (int value) {
-		
+		total = total * value;
+		output = output + " * ";
+		output = output + value;
 	}
 	
 	public void divide (int value) {
-		
+		if (value == 0){
+			total = 0;
+			output = output + " / ";
+			output = output + value;
+		}
+		else{
+			total = total/value;
+			output = output + " / ";
+			output = output + value;
+		}	
 	}
 	
 	public String getHistory () {
-		return "";
+		return output;
 	}
 }
